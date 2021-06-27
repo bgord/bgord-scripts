52487d46dbeadbc2db2b40ec610bc05e2efc7198
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-27 18:00:23 +0200

    step: add setup-eslint script

---
+++ toolbox/setup-eslint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+
+info "Installing ESLint dependencies..."
+npm install -D @typescript-eslint/{parser,eslint-plugin} eslint eslint-config-{bgord,prettier} eslint-plugin-{jsx-a11y,react,react-hooks,sonarjs}
+
+success "ESLint dependencies installed successfully!"
