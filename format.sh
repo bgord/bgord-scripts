5480677c0a1aa956bf10414f9473aedc4551d93a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-27 17:32:33 +0200

    feature: add format script

---
+++ format.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+
+info "Formatting all files..."
+npx pretty-quick
+
+success "Files formatted successfully!"
