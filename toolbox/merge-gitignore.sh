8f7e4e6d29c0cb9f99f6f33189371a4fe85fea3b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-27 18:52:12 +0200

    fix: redirect sort output to .gitignore

--- toolbox/merge-gitignore.sh
+++ toolbox/merge-gitignore.sh
@@ -6,6 +6,6 @@ source bgord-scripts/base.sh
 check_if_file_exists .gitignore
 
 info "Merging .gitignore..."
-sort --unique bgord-scripts/.gitignore .gitignore
+sort --unique bgord-scripts/.gitignore .gitignore > .gitignore
 
 success ".gitignore merged correctly!"
