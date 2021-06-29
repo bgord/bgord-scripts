8fcfc1d897ede20ba50beeba3093b46b8aa38ef0
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-29 22:11:55 +0200

    step: add pre-push to setup-husky

--- toolbox/setup-husky.sh
+++ toolbox/setup-husky.sh
@@ -8,5 +8,6 @@ npm install -D husky
 npm set-script prepare "husky install"
 npm run prepare
 npx husky add .husky/pre-commit "./bgord-scripts/pre-commit.sh"
+npx husky add .husky/pre-push "./bgord-scripts/pre-push.sh"
 
 success "Husky set up!"
