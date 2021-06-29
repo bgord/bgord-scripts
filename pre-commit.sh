3be3fc072fec32ba83fbfe603c4c692f90d64836
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-29 21:53:28 +0200

    feature: add pre-commit hook

---
+++ pre-commit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Running pre-commit hook..."
+./bgord-scripts/format:staged.sh
+./bgord-scripts/lint.sh
+
+success "Pre-commit hook has finished!"
