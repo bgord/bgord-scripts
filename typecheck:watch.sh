60f7f12764f5e028d0900063a4d24a34f5f444c4
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-07 21:53:15 +0200

    step: add typecheck:watch

---
+++ typecheck:watch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Typechecking continuously..."
+npx tsc --watch --project tsconfig.json --noEmit
