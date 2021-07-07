c7afab14d3e0ae6a2ffae534f6f9e0c8db0ff73c
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-07 22:00:41 +0200

    step: add preserveWatchOutput to typecheck:watch

--- typecheck:watch.sh
+++ typecheck:watch.sh
@@ -5,4 +5,4 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Typechecking continuously..."
-npx tsc --watch --project tsconfig.json --noEmit
+npx tsc --watch --preserveWatchOutput --project tsconfig.json --noEmit
