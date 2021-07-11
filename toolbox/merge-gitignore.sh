5a8703e99e5b797977fc455451bdc047d0cdeeb7
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-11 17:24:13 +0200

    fix: merge-gitignore script

--- toolbox/merge-gitignore.sh
+++ toolbox/merge-gitignore.sh
@@ -4,9 +4,16 @@
 source bgord-scripts/base.sh
 setup_base_config
 
+TMP_GITIGNORE_FILEPATH="/tmp/.bgord-scripts-gitignore"
+
 check_if_file_exists .gitignore
 
 info "Merging .gitignore..."
-sort --unique bgord-scripts/.gitignore .gitignore > .gitignore
+
+sort --unique bgord-scripts/.gitignore .gitignore \
+  | grep -v '#' \
+  | tail +2 \
+  > $TMP_GITIGNORE_FILEPATH \
+  && mv $TMP_GITIGNORE_FILEPATH .gitignore
 
 success ".gitignore merged correctly!"
