d33092c7c4eb5b38948c8ecc9bfd997cdfffbc2b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-12 20:45:57 +0200

    step: add info steps for setup-prisma-for-sqlite

--- toolbox/setup-prisma-for-sqlite.sh
+++ toolbox/setup-prisma-for-sqlite.sh
@@ -11,4 +11,8 @@ install_dev_package prisma
 mkdir prisma
 cp bgord-scripts/templates/schema-sqlite.prisma prisma/schema.prisma
 
+info "1. Edit your data model in prisma/schema.prisma"
+info "2. Run 'npx prisma generate' to generate Prisma client for your data model"
+info "3. Run 'npx prisma migrate dev' to create your first migration"
+
 success "Prisma for SQLite set up!"
