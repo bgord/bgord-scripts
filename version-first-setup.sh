27bc771ddc371ac73d33a7e6cd9696cae17f5a20
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-22 20:52:11 +0100

    fix: use inline standard-version types

--- version-first-setup.sh
+++ version-first-setup.sh
@@ -5,6 +5,6 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Creating first version..."
-npx standard-version --first-release --no-verify --preset ./bgord-scripts/.versionrc.json
+npx standard-version --first-release --no-verify --types '[{"type":"feature","section":"Features"},{"type":"fix","section":"Bug Fixes"},{"type":"step","hidden":true},{"type":"docs","hidden":true},{"type":"refactor","hidden":true},{"type":"style","hidden":true},{"type":"test","hidden":true}]'
 
 success "First version created!"
