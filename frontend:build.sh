3a40281f7ecbe3cea6b9a74299f2e2863e0b1260
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-02-17 00:02:49 +0100

    feature: define frontend build version and date

--- frontend:build.sh
+++ frontend:build.sh
@@ -4,12 +4,16 @@
 source bgord-scripts/base.sh
 
 info "Environment: staging"
+
 info "Building frontend..."
 
-# ==========================================================
+BUILD_VERSION=\"$(node -p -e "require('./package.json').version")\"
+BUILD_DATE=$(date +"%s")
 
 npx esbuild frontend/index.tsx \
   --bundle \
   --minify \
+  --define:BUILD_VERSION=$BUILD_VERSION \
+  --define:BUILD_DATE=$BUILD_DATE \
   --outdir=static/ \
   $@
--- frontend:watch.sh
+++ frontend:watch.sh
@@ -5,8 +5,14 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Watching and rebuilding frontend..."
+
+BUILD_VERSION=\"$(node -p -e "require('./package.json').version")\"
+BUILD_DATE=$(date +"%s")
+
 npx esbuild \
   --bundle \
+  --define:BUILD_VERSION=$BUILD_VERSION \
+  --define:BUILD_DATE=$BUILD_DATE \
   --watch frontend/index.tsx \
   --outdir=static/ \
   $@
