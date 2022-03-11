c377ec868f677c5f5fabc7926241a1c9e7d58f21
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-11 15:48:38 +0100

    fix: build version

--- frontend:watch.sh
+++ frontend:watch.sh
@@ -6,7 +6,7 @@ setup_base_config
 
 info "Watching and rebuilding frontend..."
 
-BUILD_VERSION=\"$(node -p -e "require('./package.json').version")\"
+BUILD_VERSION=\"v$(node -p -e "require('./package.json').version")\"
 BUILD_DATE=$(date +"%s")
 
 npx esbuild \
