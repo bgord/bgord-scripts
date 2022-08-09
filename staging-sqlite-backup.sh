24de01b3447c9cc9daa219890b8b54c234479958
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-09 20:44:24 +0200

    feature: add backup rotation

--- staging-sqlite-backup.sh
+++ staging-sqlite-backup.sh
@@ -6,7 +6,8 @@ PROJECT_NAME=$1
 CURRENT_TIME=$(date +%F-%H-%M-%S)
 
 DATABASE_PATH="/var/www/$PROJECT_NAME/sqlite.db"
-BACKUP_PATH="/var/backups/$PROJECT_NAME/$CURRENT_TIME.sqlite.backup"
+BACKUPS_PATH="/var/backups/$PROJECT_NAME"
+BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.sqlite.backup"
 
 echo "Creating a backup of database: $DATABASE_PATH."
 sqlite3 $DATABASE_PATH ".backup $BACKUP_PATH"
@@ -14,3 +15,6 @@ echo "Backup created to: $BACKUP_PATH."
 
 gzip $BACKUP_PATH
 echo "Backup compressed."
+
+find $BACKUPS_PATH/* -mtime +7 -exec rm {} \;
+echo "Files older than 7 days deleted"
