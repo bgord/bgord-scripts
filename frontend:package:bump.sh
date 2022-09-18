f23d6255f61f19428d40e5e07ff2cd055f6e7931
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-09-18 23:50:50 +0200

    feature: add frontend:package:bump script

---
+++ frontend:package:bump.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+
+info "Installing new @bgord/frontend package version"
+
+npm install @bgord/frontend
