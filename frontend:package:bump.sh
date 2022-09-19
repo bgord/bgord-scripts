79ad1b1fd2aa06ffed861f4c9284980224f47f64
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-09-19 18:45:51 +0200

    step: add --no-verify to frontend package bump

--- frontend:package:bump.sh
+++ frontend:package:bump.sh
@@ -10,4 +10,4 @@ npm install @bgord/frontend
 info "Commiting package.json changes"
 
 git add package.json package-lock.json
-git commit -m "step: bump bgord/frontend"
+git commit -m "step: bump bgord/frontend" --no-verify
