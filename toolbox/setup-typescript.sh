8a26f34761b50962e6a6915087d1be36b1c4f36a
Bartosz Gordon <gordonbartosz@gmail.com>
2023-02-04 15:53:25 +0100

    feature: add setup typescript script

---
+++ toolbox/setup-typescript.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Installing TypeScript dependency..."
+npm i -D typescript@^4.8.3
+cp ./bgord-scripts/templates/tsconfig.json .
+
+success "TypeScript set up successfully!"
