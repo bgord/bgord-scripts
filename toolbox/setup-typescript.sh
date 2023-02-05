1d6e7dcffb1a3d4756b67194a34aa8b1e4594727
Bartosz Gordon <gordonbartosz@gmail.com>
2023-02-05 14:33:43 +0100

    fix: use fixed ts version

--- toolbox/setup-typescript.sh
+++ toolbox/setup-typescript.sh
@@ -5,7 +5,7 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Installing TypeScript dependency..."
-npm i -D typescript@^4.8.3
+npm i -D typescript@4.8.3
 cp ./bgord-scripts/templates/tsconfig.json .
 
 success "TypeScript set up successfully!"
