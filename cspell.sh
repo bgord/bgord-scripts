9642f2a1fb50cadf9a98c27f0fab5c2aa072a1ae
Bartosz Gordon <gordonbartosz@gmail.com>
2023-10-10 20:53:51 +0200

    step: ignore more files in cspell

--- cspell.sh
+++ cspell.sh
@@ -10,6 +10,8 @@ git ls-files \
   | grep -v "package.json" \
   | grep -v "prisma" \
   | grep -v "infra/translations/pl.json" \
+  | grep -v "dist" \
+  | grep -v "LICENSE" \
   | npx cspell \
   --file-list stdin \
   --locale "en" \
