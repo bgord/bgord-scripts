1431f76eb48b3de734d0d6a85cc088965d41b733
Bartosz Gordon <gordonbartosz@gmail.com>
2023-10-10 16:16:05 +0200

    feature: add cspell.sh script

---
+++ cspell.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Detecting spelling mistakes..."
+
+git ls-files \
+  | grep -v "package.json" \
+  | grep -v "prisma" \
+  | grep -v "infra/translations/pl.json" \
+  | npx cspell \
+  --file-list stdin \
+  --locale "en" \
+  --unique \
+  --no-progress \
+  --cache \
+  --config "bgord-scripts/cspell.json" \
+  $@
+
+success "Files are correct!"
