05644b5461b3678fc85330f85ec574c945d998ac
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-25 13:51:08 +0100

    step: add shellcheck to pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,5 +8,6 @@ info "Running pre-commit hook..."
 ./bgord-scripts/format-staged.sh
 ./bgord-scripts/lint.sh
 ./bgord-scripts/cspell.sh
+./bgord-scripts/shellcheck.sh
 
 success "Pre-commit hook has finished!"
