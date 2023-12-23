6c0d7ebe855f8a006a59756a778df69d72f48ff8
Bartosz Gordon <gordonbartosz@gmail.com>
2023-12-23 23:09:33 +0100

    feature: add knip config file

--- typecheck-prune.sh
+++ typecheck-prune.sh
@@ -6,15 +6,4 @@ setup_base_config
 
 info "Detecting unused code..."
 
-npx ts-prune \
-  | grep -v -e '(used in module)' \
-    -e "db.ts" \
-    -e "policies/index.ts" \
-    -e "services/index.ts" \
-    -e "value-objects/index.ts" \
-    -e "infra/index.ts" \
-  || true
-
-npx knip --strict --no-exit-code
-
-# Force exit code 0
+npx knip --strict --no-exit-code --config bgord-scripts/knip.json
