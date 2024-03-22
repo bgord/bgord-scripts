712f4928bf03ddbd509c0d0dfcb79ba89c7352e3
Bartosz Gordon <gordonbartosz@gmail.com>
2024-03-22 10:20:49 +0100

    feature: add image svg optimise script

---
+++ image-svg-optmise.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+
+info "Identifying image..."
+
+IMAGE_PATH=$1
+
+validate_non_empty "IMAGE_PATH" $IMAGE_PATH
+
+npx svgo $IMAGE_PATH
