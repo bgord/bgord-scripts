2b99f387172c9d4279d51265d1a2f0cf5bf6db28
Bartosz Gordon <gordonbartosz@gmail.com>
2024-05-11 12:37:33 +0200

    feature: add lint fix safe/unsafe

---
+++ lint-fix-safe.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Fixing safe lint errors..."
+
+npx @biomejs/biome lint --config-path=bgord-scripts/biome.json --apply .
+
+success "Safe lint errors fixed!"
