cfcee21ccd26f5d24ac7dc5ae93a134a148ff60b
Bartosz Gordon <gordonbartosz@gmail.com>
2024-05-13 18:57:19 +0200

    feature: add test-project-start script

---
+++ test-project-start.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+
+if test -f "scripts/test-project-start.sh"
+then
+  info "Test project start script exists"
+  bash scripts/test-project-start.sh
+  exit
+else
+  info "Test project start script does not exist"
+fi
+
+info "Environment: test"
+info "Starting project..."
+
+# ==========================================================
+
+cp node_modules/@bgord/design/dist/main.min.css static/
+cp node_modules/@bgord/design/dist/normalize.min.css static/
+info "Copied CSS from @bgord/design"
+
+# ==========================================================
+
+./bgord-scripts/frontend-build.sh
+info "Built frontend"
+
+# ==========================================================
+
+export NODE_ENV="test"
+
+npx serve --listen 3333 .
