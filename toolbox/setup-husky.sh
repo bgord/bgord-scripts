62fb6aea61c9124c8445e8ae4814d8a78a164c24
Bartosz Gordon <gordonbartosz@gmail.com>
2024-08-24 16:47:05 +0200

    fix: setup husky

--- toolbox/setup-husky.sh
+++ toolbox/setup-husky.sh
@@ -4,9 +4,9 @@
 source bgord-scripts/base.sh
 setup_base_config
 
-# info "Installing Husky..."
-# npm i -D husky@9.1.5
-# npx husky init
+info "Installing Husky..."
+npm i -D husky@9.1.5
+npx husky init
 echo "./bgord-scripts/pre-commit.sh" > .husky/pre-commit
 echo "./bgord-scripts/pre-push.sh" > .husky/pre-push
 echo "./bgord-scripts/commit-msg.sh" > .husky/commit-msg
