5f93f82ca39fe999798ed0652ae55a045a726367
Bartosz Gordon <gordonbartosz@gmail.com>
2024-09-14 15:48:55 +0200

    refactor: pinpoint commitlint versions in setup commitlint

--- toolbox/setup-commitlint.sh
+++ toolbox/setup-commitlint.sh
@@ -6,7 +6,8 @@ setup_base_config
 
 info "Installing commitlint dependencies..."
 
-install_dev_package @commitlint/{cli,config-conventional}
+install_dev_package @commitlint/cli@19.5.0
+install_dev_package @commitlint/config-conventional@19.5.0
 
 info "Run './bgord-scripts/setup-husky.sh' to add the commit-msg git hook"
 info "WARNING: you may want to double-check if other hooks were not rewritten!"
