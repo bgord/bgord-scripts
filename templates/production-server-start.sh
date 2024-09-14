42f364f80cfe73b65b79584f9e340bdcb32c8c07
Bartosz Gordon <gordonbartosz@gmail.com>
2024-09-14 15:43:35 +0200

    fix: production server start template

--- templates/production-server-start.sh
+++ templates/production-server-start.sh
@@ -5,7 +5,7 @@ echo "Starting project..."
 
 export NODE_ENV="production"
 
-cd /var/www/trackers/ || exit
+cd /var/www/{{PROJECT_NAME}}/ || exit
 npx prisma migrate deploy
 npx prisma generate
 node --require tsx/cjs index.js
