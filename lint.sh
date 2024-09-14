d415aa690336044b9cdfaa09c4f9f1f2f83a6047
Bartosz Gordon <gordonbartosz@gmail.com>
2024-09-14 16:26:53 +0200

    refactor: move biome config to templates

--- lint.sh
+++ lint.sh
@@ -5,6 +5,6 @@ setup_base_config
 
 info "Linting all files..."
 
-npx @biomejs/biome lint --config-path=bgord-scripts/biome.json .
+npx @biomejs/biome lint --config-path=bgord-scripts/templates/biome.json .
 
 success "Files are correct!"
