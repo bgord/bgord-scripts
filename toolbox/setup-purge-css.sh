b49d3a7ee38c89add95ea2f42a3fa582b762ebff
Bartosz Gordon <gordonbartosz@gmail.com>
2024-09-14 15:39:07 +0200

    refactor: update setup-purge-css

--- toolbox/setup-purge-css.sh
+++ toolbox/setup-purge-css.sh
@@ -5,6 +5,8 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Installing Purge CSS..."
-npm install -D @fullhuman/postcss-purgecss postcss{,-cli}
+npm install -D @fullhuman/postcss-purgecss@6.0.0
+npm install -D postcss@8.4.45,
+npm install -D postcss-cli@11.0.0
 
 success "Purge CSS dependencies installed successfully!"
