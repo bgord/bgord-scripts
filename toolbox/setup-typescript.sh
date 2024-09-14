7da21b0038a8a83c0bbf978befc26cc0adb5e94f
Bartosz Gordon <gordonbartosz@gmail.com>
2024-09-14 15:33:42 +0200

    step: update setup typescript script

--- toolbox/setup-typescript.sh
+++ toolbox/setup-typescript.sh
@@ -5,7 +5,7 @@ source bgord-scripts/base.sh
 setup_base_config
 
 info "Installing TypeScript dependency..."
-npm i -D typescript@4.8.3
+npm i -D typescript@5.6.2
 cp ./bgord-scripts/templates/tsconfig.json .
 
 success "TypeScript set up successfully!"
