0b02fad986bf32eabecce40f3a0120caa6d62d4c
Bartosz Gordon <gordonbartosz@gmail.com>
2024-10-12 21:14:41 +0200

    step: add bun-prisma-validate

---
+++ bun-prisma-validate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+SCHEMA_FILE="prisma/schema.prisma"
+
+# Check if the schema file exists
+if [ -f "$SCHEMA_FILE" ]; then
+  info "Validating Prisma schema file at $SCHEMA_FILE..."
+
+  bunx prisma validate
+
+  success "No Prisma schema validation errors!"
+else
+  info "Prisma schema file - $SCHEMA_FILE - not found. Validation skipped."
+fi
