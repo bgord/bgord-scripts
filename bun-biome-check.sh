9ee5973b960b5694f1e2f0b46540dfb05cbaf49a
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-13 13:59:32 +0100

    feature: add bun biome check

---
+++ bun-biome-check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Performing biome analysis..."
+
+bunx @biomejs/biome check \
+    --config-path=bgord-scripts/templates/biome.json \
+    --write
+
+success "Biome analysis performed!"
