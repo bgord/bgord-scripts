f4127b061d315892f66a746edf54e27fb236fde8
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-17 21:18:28 +0100

    step: add bun-frontend-build

--- bun-frontend-build.sh
+++ bun-frontend-build.sh
@@ -9,12 +9,22 @@ info "Building frontend..."
 BUILD_VERSION=\"$(node -p -e "require('./package.json').version")\"
 BUILD_DATE=$(date +"%s")
 
-bunx esbuild frontend/index.tsx \
-  --bundle \
-  --minify \
-  --define:BUILD_VERSION=$BUILD_VERSION \
-  --define:BUILD_DATE=$BUILD_DATE \
-  --outdir=static/ \
-  $@
+bun build frontend/index.tsx
+--minify
+--target=browser
+--tree-shaking
+--splitting
+--chunks
+--sourcemap=none
+--define process.env.NODE_ENV=\"production\"
+--define 'console.log=undefined'
+--define 'console.debug=undefined'
+--define 'console.info=undefined'
+--no-summary
+--jsx-optimize
+--define BUILD_VERSION=$BUILD_VERSION
+--define BUILD_DATE=$BUILD_DATE
+--outdir static/
+$@
 
 success "Frontend built!"
