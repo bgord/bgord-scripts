fb89afe5ba5347161d55ba1b08881ef1d7a7aff8
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-23 13:44:42 +0100

    step: add lefthook target

--- toolbox/setup-lefthook.sh
+++ toolbox/setup-lefthook.sh
@@ -7,7 +7,7 @@ info "Installing Lefthook..."
 
 install_dev_package lefthook@1.8.2
 
-cp bgord-scripts/templates/lefthook.yml lefthook.yml
+cp bgord-scripts/templates/lefthook-target.yml lefthook.yml
 bunx lefthook install
 
 success "Lefthook set up!"
