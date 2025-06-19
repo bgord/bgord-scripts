b52e73b96bcebae0692d1e38c467f26059df136b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-19 22:35:33 +0200

    feature: add app-readme-generate script

---
+++ app-readme-generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+OUTPUT_FILE="readme.md"
+
+PROJECT_NAME=$1
+
+validate_non_empty "PROJECT_NAME" $PROJECT_NAME
+
+echo "# $PROJECT_NAME" > "$OUTPUT_FILE"
+echo "" >> "$OUTPUT_FILE"
+
+echo "## Domain:" >> "$OUTPUT_FILE"
+echo "" >> "$OUTPUT_FILE"
+
+tree modules/ | grep -v index.ts | sed -e :a -e '$d;N;2,2ba' -e 'P;D' >> "$OUTPUT_FILE"
