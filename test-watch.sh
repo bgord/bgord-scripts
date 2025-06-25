af8b561372eec84be666b1260ea3124dbd165d28
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-25 16:00:47 +0200

    refactor: remove legacy test scripts

--- test-watch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source bgord-scripts/base.sh
-setup_base_config
-set_node_timezone_to_utc
-
-info "Running tests in watch mode..."
-
-npx vitest watch --passWithNoTests --exclude infra/e2e
