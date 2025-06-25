41d558d099f66c04e30048cf62ad1fd11cb4b5ae
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-25 16:01:46 +0200

    refactor: remove legacy typecheck scripts

--- typecheck-watch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source bgord-scripts/base.sh
-setup_base_config
-
-info "Typechecking continuously..."
-
-npx tsc --watch --preserveWatchOutput --project tsconfig.json --noEmit
