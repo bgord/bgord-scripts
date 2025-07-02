1567587813c2ebfe87b624566db74ee434797405
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 19:26:41 +0200

    step: update typecheck-frontend

--- typecheck-frontend.sh
+++ typecheck-frontend.sh
@@ -3,11 +3,20 @@
 source bgord-scripts/base.sh
 setup_base_config
 
-info "Typechecking..."
+info "Typechecking frontend..."
 
-cd frontend/
-bunx react-router typegen
-cd ../
-bunx tsc --project frontend/tsconfig.json --noEmit
+FRONTEND_DIRECTORY="frontend/"
 
-success "Types are correct!"
+if test -d "$FRONTEND_DIRECTORY"
+then
+  cd $FRONTEND_DIRECTORY
+
+  bunx react-router typegen
+
+  bunx tsc --project tsconfig.json --noEmit
+
+  success "Types are correct!"
+else
+  error "$FRONTEND_DIRECTORY directory doesn't exist!"
+  exit 1
+fi
