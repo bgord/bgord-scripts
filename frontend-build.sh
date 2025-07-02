6f7dbc3a123516921dd29f37f24a7ec461b7a092
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 16:31:44 +0200

    fix: missing cd

--- frontend-build.sh
+++ frontend-build.sh
@@ -9,6 +9,7 @@ info "Building frontend..."
 cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
 cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
 
+cd frontend/
 bunx react-router build
 
 success "Frontend built!"
--- frontend-watch.sh
+++ frontend-watch.sh
@@ -8,5 +8,5 @@ info "Watching and rebuilding frontend..."
 cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
 cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
 
-cd frontend
+cd frontend/
 bunx react-router dev
