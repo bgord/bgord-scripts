58dfbecf8b3252ae13f170f63e0c3aa3d8a91948
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 16:31:18 +0200

    refactor: fix missing bunx

--- frontend-build.sh
+++ frontend-build.sh
@@ -9,6 +9,6 @@ info "Building frontend..."
 cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
 cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
 
-react-router build
+bunx react-router build
 
 success "Frontend built!"
