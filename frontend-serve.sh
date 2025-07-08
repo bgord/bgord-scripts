b4730d25cdb459da767a8dd3c95c61a67e726743
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-08 12:55:48 +0200

    step: add frontend-serve script

---
+++ frontend-serve.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+info "Environment: production"
+info "Serving frontend..."
+
+cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
+cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
+
+bunx react-router-serve ./frontend/build/server/index.js
