6d02814e9affc6252b35959e5773b12e5b569d68
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-20 23:59:53 +0200

    step: run frontend scripts in bun

--- frontend-build.sh
+++ frontend-build.sh
@@ -10,6 +10,6 @@ cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
 cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
 
 cd frontend/
-bunx react-router build
+bunx --bun react-router build
 
 success "Frontend built!"
--- frontend-watch.sh
+++ frontend-watch.sh
@@ -9,4 +9,4 @@ cp frontend/node_modules/@bgord/design/dist/main.min.css frontend/public/
 cp frontend/node_modules/@bgord/design/dist/normalize.min.css frontend/public/
 
 cd frontend/
-bunx react-router dev
+bunx --bun react-router dev
