fdba3a4cfbc685af2ee9ed393cf4c2c4418b3674
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-28 23:51:31 +0200

    fix: verify deps

--- verify-dependencies.sh
+++ verify-dependencies.sh
@@ -5,29 +5,22 @@ setup_base_config
 
 info "Verifying dependencies"
 
-function check_dependencies() {
-  local package_json_path="package.json"
-  local dep_type=$1
+check_if_file_exists "package.json"
 
-  if ! jq -e ".$dep_type" "$package_json_path" >/dev/null; then
-    return
-  fi
-
-  while read -r name version; do
-    if test "$version" == *"^"* || test "$version" == *"~"*; then
-      error "$name@$version in $package_json_path is not pinned"
-      exit 1
-    fi
-
-    if test "$version" == *"file:"*; then
-      error "$name@$version in $package_json_path is not pinned"
-      exit 1
-    fi
-  done < <(jq -r ".$dep_type | to_entries | .[] | .key + \" \" + .value" "$package_json_path")
-}
-
-check_dependencies "dependencies"
-check_dependencies "devDependencies"
-check_dependencies "peerDependencies"
+# Fail if any version in deps/devDeps/peerDeps starts with '^'
+if CARET_DEPS=$(jq -r '
+  def e(section):
+    .[section] // {}
+    | to_entries[]
+    | select(.value | type=="string" and startswith("^"))
+    | "\(.key)=\(.value)\t[" + section + "]";
+  e("dependencies"),
+  e("devDependencies"),
+  e("peerDependencies")
+' package.json); [[ -n "${CARET_DEPS}" ]]; then
+  error "Found caret (^) versions:"
+  echo "${CARET_DEPS}" | sed 's/^/  - /'
+  exit 1
+fi
 
 success "Dependencies verified"
