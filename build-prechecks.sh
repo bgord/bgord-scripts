d964171995356851d79fb78fd6e9189844987818
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-17 17:36:28 +0200

    step: update build prechecks

--- build-prechecks.sh
+++ build-prechecks.sh
@@ -6,9 +6,14 @@ setup_base_config
 info "Running build prechecks..."
 
 ./bgord-scripts/typecheck.sh
-./bgord-scripts/biome-check.sh
+./bgord-scripts/typecheck-prune.sh
+./bgord-scripts/typecheck-frontend.sh
 ./bgord-scripts/test-run.sh
-./bgord-scripts/cspell.sh
-./bgord-scripts/shellcheck.sh
+./bgord-scripts/outdated-dependencies.sh
 ./bgord-scripts/compare-configs.sh
+./bgord-scripts/verify-dependencies.sh
+./bgord-scripts/depcruise.sh
+./bgord-scripts/cspell.sh
+./bgord-scripts/biome-check.sh
+./bgord-scripts/shellcheck.sh
 ./bgord-scripts/e2e-run.sh
