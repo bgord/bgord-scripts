a74eed478c3216cebfd19c9c9dfb7540705b1866
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-22 15:01:39 +0200

    refactor: remove cspell from build prechecks

--- build-prechecks.sh
+++ build-prechecks.sh
@@ -13,7 +13,6 @@ info "Running build prechecks..."
 ./bgord-scripts/compare-configs.sh
 ./bgord-scripts/verify-dependencies.sh
 ./bgord-scripts/depcruise.sh
-./bgord-scripts/cspell.sh
 ./bgord-scripts/biome-check.sh
 ./bgord-scripts/shellcheck.sh
 ./bgord-scripts/e2e-run.sh
