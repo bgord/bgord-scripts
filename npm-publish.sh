aab71f3fc8ccffe641fa5e2d913547303e84ae6f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-23 12:56:27 +0200

    refactor: rewrite npm publish

--- npm-publish.sh
+++ npm-publish.sh
@@ -10,68 +10,54 @@ CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
 # ========================================
 
+step_start "Version change"
 validate_non_empty "VERSION_CHANGE" $VERSION_CHANGE
 
 if test "$VERSION_CHANGE" == "major" || test "$VERSION_CHANGE" == "minor" || test "$VERSION_CHANGE" == "patch"
 then
-  success "Version change: $VERSION_CHANGE"
+  info "Version change: $VERSION_CHANGE"
 else
   error "Unsupported version change: $VERSION_CHANGE"
   exit 1
 fi
+step_end "Version change"
 
-# ========================================
-
+step_start "Sync status"
 if test $(git rev-parse master) != $(git rev-parse origin/master)
 then
     error "There are some differences between master and origin/master"
     info "Please, sync them"
     exit 1
 fi
-success "All changes are pushed to the remote master branch"
-
-# ========================================
-
-info "Building the package"
+step_end "Sync status"
 
 bun run build
 
-success "Package built successfully"
-
-# ========================================
-
+step_start "NPM login status"
 if test ! $(npm whoami)
 then
   error "You're not logged in to npm"
   info "Run [npm login], you can find the credentials on Keeper"
   exit 1
 fi
-success "You're logged in to npm"
+step_end "NPM login status"
 
-# ========================================
-
-info "Changing app $VERSION_CHANGE version..."
+step_start "Version bump: $VERSION_CHANGE"
 npm version $VERSION_CHANGE
-success "Version changed to $VERSION_CHANGE!"
+step_end "Version bump: $VERSION_CHANGE"
 
-# ========================================
-
-info "Pushing version change..."
+step_start "Tags push"
 git push --no-verify
 git push --tags --no-verify
-success "Version change pushed!"
+step_end "Tags push"
 
-# ========================================
-
+step_start "Publish dry run"
 npm publish --dry-run
-success "Ran npm publish --dry-run"
-
-# ========================================
+step_end "Publish dry run"
 
 info "About to run [npm publish] after you press Enter"
-
 press_enter_to_continue
 
+step_start "Publish"
 npm publish
-
-success "The package has been published!"
+step_end "Publish"
