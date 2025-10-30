26ea8b0d452b4b3204f4fb270f56c979b2e6b592
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-30 09:39:27 +0100

    step: look into web not frontend in cloc

--- cloc.sh
+++ cloc.sh
@@ -4,5 +4,5 @@ source bgord-scripts/base.sh
 setup_base_config
 
 step_start "Lines of code count"
-bunx cloc index.ts server.ts infra/ modules/ app/ frontend/ --exclude-ext=js,json,sql,md,svg
+bunx cloc index.ts server.ts infra/ modules/ app/ web/ --exclude-ext=js,json,sql,md,svg
 step_end "Lines of code count"
