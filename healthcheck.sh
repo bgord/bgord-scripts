ea8f5971bffa6d92b3d99420c75d371e7f923c81
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-05 15:39:55 +0100

    feature: add healthcheck script

---
+++ healthcheck.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+BASIC_AUTH_HEADER=$1
+
+validate_non_empty "PROJECT_NAME" $PROJECT_NAME
+validate_non_empty "BASIC_AUTH_HEADER" $BASIC_AUTH_HEADER
+
+step_start "Healthcheck request"
+http GET https://$PROJECT_NAME.bgord.dev/api/healthcheck Authorization:"Basic $BASIC_AUTH_HEADER"
+step_end "Healthcheck request"
