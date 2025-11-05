213e4a4b04a1a98dde803af5c515ab1181837c8f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-05 18:05:27 +0100

    refactor: rewrite the healthcheck with curl

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,9 +6,23 @@ setup_base_config
 PROJECT_NAME=$1
 BASIC_AUTH_HEADER=$2
 
-validate_non_empty "PROJECT_NAME" $PROJECT_NAME
-validate_non_empty "BASIC_AUTH_HEADER" $BASIC_AUTH_HEADER
+validate_non_empty "PROJECT_NAME" "$PROJECT_NAME"
+validate_non_empty "BASIC_AUTH_HEADER" "$BASIC_AUTH_HEADER"
 
 step_start "Healthcheck request"
-http GET https://$PROJECT_NAME.bgord.dev/api/healthcheck Authorization:"Basic $BASIC_AUTH_HEADER"
+
+RESPONSE="$(curl -sS -H "Authorization: Basic $BASIC_AUTH_HEADER" \
+  -w $'\n%{http_code}\n' \
+  "https://${PROJECT_NAME}.bgord.dev/api/healthcheck")"
+
+STATUS="$(printf "%s" "$RESPONSE" | tail -n1)"
+BODY="$(printf "%s" "$RESPONSE" | sed -e '$d')"
+
+echo "Status: $STATUS"
+echo "$BODY"
+
 step_end "Healthcheck request"
+
+if test "$STATUS" != "200" || "$STATUS" != "429"; then
+  exit 1
+fi
