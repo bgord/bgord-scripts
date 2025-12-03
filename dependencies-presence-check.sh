f208314ed82adfefa6fdb6a5d91b58b8431c074e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-03 11:38:39 +0100

    step: add dependencies presence check

---
+++ dependencies-presence-check.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+step_start "Dependencies presence check"
+
+check_if_file_exists "package.json"
+
+check_if_package_installed "@biomejs/biome"
+check_if_package_installed "@commitlint/cli"
+check_if_package_installed "@commitlint/config-conventional"
+check_if_package_installed "@types/bun"
+check_if_package_installed "cspell"
+check_if_package_installed "knip"
+check_if_package_installed "lefthook"
+check_if_package_installed "only-allow"
+check_if_package_installed "shellcheck"
+check_if_package_installed 'typescript'
+
+step_end "Dependencies presence check"
