b7f39e1a48b947deb3a9cbf642ae26722b900a0f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-15 19:21:56 +0100

    step: improve running gitleaks

--- gitleaks-scan-staged.sh
+++ gitleaks-scan-staged.sh
@@ -3,6 +3,8 @@
 source bgord-scripts/base.sh
 setup_base_config
 
+check_if_binary_exists "gitleaks"
+
 step_start "Gitleaks scan staged"
-bunx gitleaks protect --staged --verbose --redact --config bgord-scripts/templates/gitleaks.toml
+gitleaks protect --staged --verbose --redact --config bgord-scripts/templates/gitleaks.toml
 step_end "Gitleaks scan staged"
