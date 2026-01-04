8833bda4e399161287128ef158eaa688f86f987d
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-04 15:57:19 +0100

    step: add test mutation run

---
+++ test-mutation-run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+set_node_timezone_to_utc
+
+export NODE_ENV="test"
+
+step_start "Mutation test"
+bunx stryker run
+step_end "Mutation test"
