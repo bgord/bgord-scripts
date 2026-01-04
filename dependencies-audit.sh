06f6a82703a51d21fd80b3ea523d817cbcc002e2
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-04 16:06:41 +0100

    step: add deps audit

---
+++ dependencies-audit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source bgord-scripts/base.sh
+setup_base_config
+
+step_start "Dependencies audit"
+if ! bun audit; then
+  exit 1
+fi
+success "No security vulnerabilities found."
+step_end "Dependencies audit"
