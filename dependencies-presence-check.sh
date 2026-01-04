377984be6f918878ef9a348f62e66769d7fbe7a2
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-04 15:56:09 +0100

    step: add stryker to deps presence check

--- dependencies-presence-check.sh
+++ dependencies-presence-check.sh
@@ -10,13 +10,14 @@ check_if_file_exists "package.json"
 check_if_package_installed "@biomejs/biome"
 check_if_package_installed "@commitlint/cli"
 check_if_package_installed "@commitlint/config-conventional"
+check_if_package_installed "@stryker-mutator/core"
 check_if_package_installed "@types/bun"
 check_if_package_installed "cspell"
 check_if_package_installed "knip"
 check_if_package_installed "lefthook"
+check_if_package_installed "lockfile-lint"
 check_if_package_installed "only-allow"
 check_if_package_installed "shellcheck"
-check_if_package_installed 'typescript'
-check_if_package_installed 'lockfile-lint'
+check_if_package_installed "typescript"
 
 step_end "Dependencies presence check"
