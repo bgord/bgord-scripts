45b704291c1eff9030ec232bcb9bf9e5e43970dd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-05 17:26:24 +0100

    step: add min high level to dependencies audit

--- dependencies-audit.sh
+++ dependencies-audit.sh
@@ -4,7 +4,7 @@ source bgord-scripts/base.sh
 setup_base_config
 
 step_start "Dependencies audit"
-if ! bun audit; then
+if ! bun audit --audit-level high; then
   exit 1
 fi
 success "No security vulnerabilities found."
