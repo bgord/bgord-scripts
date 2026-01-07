b065eef353d1034172998eec7bd34ab08de8bd08
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-07 11:37:02 +0100

    step: update server inspect

--- production-server-inspect.sh
+++ production-server-inspect.sh
@@ -9,10 +9,6 @@ PROJECT_NAME=$1
 
 validate_non_empty "PROJECT_NAME" "$PROJECT_NAME"
 
-step_start "Production server inspect"
-du -sh "$OUTPUT_DIRECTORY/$PROJECT_NAME"
-step_end "Production server inspect"
-
-step_start "Directory list"
+step_start "Output directory list"
 ls -al "$OUTPUT_DIRECTORY"
-step_end "Directory list"
+step_end "Output directory list"
