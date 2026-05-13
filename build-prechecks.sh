4797e34adf092db21a68701eb02b0b0908129175
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-13 13:23:29 +0200

    step: remove outdated packages

--- build-prechecks.sh
+++ build-prechecks.sh
@@ -6,7 +6,6 @@ setup_base_config
 ./bgord-scripts/typecheck.sh
 ./bgord-scripts/typecheck-prune.sh
 ./bgord-scripts/test-run.sh
-./bgord-scripts/dependencies-outdated.sh
 ./bgord-scripts/compare-configs.sh
 ./bgord-scripts/dependencies-pinpoint.sh
 ./bgord-scripts/dependencies-import-check.sh
