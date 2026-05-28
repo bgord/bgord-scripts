28bcfade4e4a5f189630daf9431ab23f10355e02
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-28 13:35:48 +0200

    step: make ci-lint scripts idempotent

--- ci-lint.sh
+++ ci-lint.sh
@@ -3,6 +3,14 @@
 source bgord-scripts/base.sh
 setup_base_config
 
+if test -d ".github/workflows"
+then
+  info "Workflows available"
+else
+  info "Workflows not available"
+  exit 0
+fi
+
 step_start "CI lint"
 zizmor --pedantic .github/
 step_end "CI lint"
