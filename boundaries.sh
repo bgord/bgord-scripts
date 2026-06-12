308c741f9344ca607c545d6a40453be46115fdf9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-12 23:00:00 +0200

    Adjust boundaries config

--- boundaries.sh
+++ boundaries.sh
@@ -3,5 +3,5 @@ source bgord-scripts/base.sh
 setup_base_config
 
 step_start "Dependencies import check"
-bunx fallow boundaries --config bgord-scripts/templates/fallow-boundaries.json
+bunx fallow dead-code --boundary-violations --config bgord-scripts/templates/fallow-boundaries.json
 step_end "Dependencies import check"
