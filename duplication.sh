7c890fd06258e898eafe340cceb089bc9133ea58
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-13 09:22:36 +0200

    Fix config path

--- duplication.sh
+++ duplication.sh
@@ -3,5 +3,5 @@ source bgord-scripts/base.sh
 setup_base_config
 
 step_start "Code duplication analysis (fallow)"
-bunx fallow dupes --config bgord-scripts/templates/fallow-dupes.json
+bunx fallow dupes --config bgord-scripts/templates/fallow-duplication.json
 step_end "Code duplication analysis (fallow)"
