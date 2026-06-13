7d8cf99a22e210223d9ba184bd45741883ecbc18
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-13 09:21:55 +0200

    Add duplication script

---
+++ duplication.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source bgord-scripts/base.sh
+setup_base_config
+
+step_start "Code duplication analysis (fallow)"
+bunx fallow dupes --config bgord-scripts/templates/fallow-dupes.json
+step_end "Code duplication analysis (fallow)"
