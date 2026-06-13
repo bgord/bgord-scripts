7b4e3742e2f139ec8269e3e997e76f2b31981174
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-13 14:10:12 +0200

    Update check secrets staged

--- check-secrets-staged.sh
+++ check-secrets-staged.sh
@@ -5,6 +5,6 @@ setup_base_config
 
 check_if_binary_exists "gitleaks"
 
-step_start "Gitleaks scan staged"
+step_start "Check secrets staged"
 gitleaks protect --staged --verbose --redact --config bgord-scripts/templates/gitleaks.toml
-step_end "Gitleaks scan staged"
+step_end "Check secrets staged"
