53874a196d5b4c021ce42872681b6dea8240b8a0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-13 10:50:51 +0200

    Remove health script

--- health.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source bgord-scripts/base.sh
-setup_base_config
-
-step_start "Complexity health analysis (fallow)"
-bunx fallow health --config bgord-scripts/templates/fallow-health.json
-step_end "Complexity health analysis (fallow)"
