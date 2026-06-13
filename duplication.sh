6b383ded20ec47f6b88508f3081a71773121556b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-13 09:40:01 +0200

    Remove duplication script

--- duplication.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source bgord-scripts/base.sh
-setup_base_config
-
-step_start "Code duplication analysis (fallow)"
-bunx fallow dupes --config bgord-scripts/templates/fallow-duplication.json
-step_end "Code duplication analysis (fallow)"
