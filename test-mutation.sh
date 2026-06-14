78ab3db8e9ec3915cf3d85bf5fd6e65c9891f9ee
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-14 13:15:15 +0200

    Rename test mutation script

--- test-mutation.sh
+++ test-mutation.sh
@@ -2,10 +2,11 @@
 
 source bgord-scripts/base.sh
 setup_base_config
+
 set_node_timezone_to_utc
 
 export NODE_ENV="test"
 
-step_start "Mutation test"
+step_start "Test mutation"
 bunx stryker run bgord-scripts/templates/stryker.config.json
-step_end "Mutation test"
+step_end "Test mutation"
