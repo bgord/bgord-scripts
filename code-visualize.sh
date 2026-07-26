e9c35e9d081d087369db00a9f567124360884621
Bartosz Gordon <gordonbartosz@gmail.com>
2026-07-26 11:40:23 +0200

    Fix code visaulize script

--- code-visualize.sh
+++ code-visualize.sh
@@ -3,8 +3,6 @@
 source bgord-scripts/base.sh
 setup_base_config
 
-ensure_web_set_up
-
 step_start "Code visualize"
 bunx fallow viz --config bgord-scripts/templates/fallow-dead-code.json
 step_end "Code visualize"
