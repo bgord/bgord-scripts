dc47128b5dbe5558c48f4e141ba012cd157ced83
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-29 17:50:48 +0200

    Switch to bgord-design css-purge

--- css-purge.sh
+++ css-purge.sh
@@ -4,5 +4,5 @@ source bgord-scripts/base.sh
 setup_base_config
 
 step_start "CSS purge"
-bunx --bun postcss-cli public/main.min.css --replace --config bgord-scripts/templates/postcss.config.mjs
+node_modules/.bin/bgord-css-purge public/main.min.css
 step_end "CSS purge"
