1e4c404ef8a43dfb741b51d17bbc43fc53dcd779
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-25 00:40:32 +0200

    Revert "Add entry filename hashing in web build bun"
    This reverts commit 3e14942f9e25457dd614fe68b86146466ac5ad08.

--- web-build-bun.sh
+++ web-build-bun.sh
@@ -7,24 +7,15 @@ ensure_web_set_up
 
 info "Environment: production"
 
-ENTRY="web/entry-client.tsx"
-
-HASH_FLAGS=()
-if test "${NODE_ENV:-}" == "production"
-then
-  HASH_FLAGS=(--entry-naming "[name]-[hash].[ext]" --metafile=meta.web.json)
-fi
-
 public_artifacts_remove
 bgord_design_copy
 
 step_start "Web build bun"
-bun build "$ENTRY" \
+bun build web/entry-client.tsx \
   --outdir ./public \
   --target browser \
   --splitting \
   --minify \
   --production \
-  --define process.env.NODE_ENV=\"production\" \
-  "${HASH_FLAGS[@]}"
+  --define process.env.NODE_ENV=\"production\"
 step_end "Web build bun"
